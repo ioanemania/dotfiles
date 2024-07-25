98fbf72fc0a905175c1f2f2e14adf7737ab42284
Ioane Mania <imaniaioane@gmail.com>
2024-07-25 12:01:13 +0400

    Add a bunch of stuff

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,14 +1,10 @@
 local wezterm = require("wezterm")
 
-wezterm.on("window-focus-changed", function()
-	os.execute(
-		"xdotool search -classname org.wezfurlong.wezterm | xargs -I{} xprop -f _KDE_NET_WM_BLUR_BEHIND_REGION 32c -set _KDE_NET_WM_BLUR_BEHIND_REGION 0 -id {}"
-	)
-end)
-
 local config = wezterm.config_builder()
 
-config.color_scheme = "Tokyo Night"
+config.enable_wayland = false
+
+config.color_scheme = "Melange"
 
 config.enable_tab_bar = false
 config.window_padding = {
@@ -18,10 +14,39 @@ config.window_padding = {
 	bottom = 0,
 }
 config.window_close_confirmation = "NeverPrompt"
-config.window_background_opacity = 0.8
+-- config.window_background_opacity = 0.90
 config.warn_about_missing_glyphs = false
 
-config.font = wezterm.font("Monaspace Radon")
-config.font_size = 17
+config.font = wezterm.font("CozetteVector", { weight = "Medium" })
+-- config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
 
+wezterm.on("window-resized", function(window, _)
+	local window_dims = window:get_dimensions()
+	local overrides = window:get_config_overrides() or {}
+
+	if not window_dims.is_full_screen then
+		if not overrides.window_padding then
+			-- not changing anything
+			return
+		end
+		overrides.window_padding = nil
+	else
+		local window_width = math.floor(window_dims.pixel_width / 1.5)
+		local padding = math.floor((window_dims.pixel_width - window_width) / 2)
+		local new_padding = {
+			left = padding,
+			right = padding,
+			top = 0,
+			bottom = 0,
+		}
+		if overrides.window_padding and new_padding.left == overrides.window_padding.left then
+			-- padding is same, avoid triggering further changes
+			return
+		end
+		overrides.window_padding = new_padding
+	end
+	window:set_config_overrides(overrides)
+end)
+
+config.font_size = 18
 return config
